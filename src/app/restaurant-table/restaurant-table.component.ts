19d2aa07f9ca445a3219b05836490203a17da48f
matthewmorse0 <66086952+matthewmorse0@users.noreply.github.com>
2022-11-14 18:40:18 -0600

    Successfully send args through url

--- src/app/restaurant-table/restaurant-table.component.ts
+++ src/app/restaurant-table/restaurant-table.component.ts
@@ -18,32 +18,21 @@ export class RestaurantTableComponent implements OnInit {
 
   private async getRest() {
 
-    // const userAction = async () => {
-    //   const response = await fetch('http://127.0.0.1:5000/view_info');
-    //   const myJson = await response.json(); //extract JSON from the http response
-    //   // do something with myJson
-    // }
-
-
-    var json = JSON.stringify({ rid: 2 });
     let xhr = new XMLHttpRequest();
-    xhr.open("GET", "http://127.0.0.1:5000/view_info");
-    xhr.send(json);
+    xhr.open("GET", "http://127.0.0.1:5000/test_view_info?rid=2");
+    xhr.send();
     xhr.onload = () => 
     {
       var data = xhr.responseText;
-      console.log(json)
-      console.log(data)
       var response = JSON.parse(data)
-      console.log(response)
       this.rest = response.rest_info as RestaurantObject
-      /*this.rest.name = this.rest[0].toString();
+      this.rest.name = this.rest[0].toString();
       this.rest.id = this.rest[1]
       this.rest.managerId = this.rest[2]
       this.rest.waitTime = this.rest[3]
       this.rest.menuLink = this.rest[4]
       this.rest.diningTables = this.rest[5]
-      this.rest.freeTables = this.rest[6]*/
+      this.rest.freeTables = this.rest[6]
       console.log(this.rest);
     }
   }
