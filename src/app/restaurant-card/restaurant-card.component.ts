1be624819bccd553e8a83ee5fd543dc8592cd3df
matthewmorse0 <66086952+matthewmorse0@users.noreply.github.com>
2022-11-26 14:01:18 -0700

    auto reloads

--- src/app/restaurant-card/restaurant-card.component.ts
+++ src/app/restaurant-card/restaurant-card.component.ts
@@ -77,5 +77,6 @@ export class RestaurantCardComponent implements OnInit {
       xhr.open("POST", "http://127.0.0.1:5000/update_layout?rid=2&tables="+tables);
       xhr.send();
     }
+    window.location.reload();
   }
 }
